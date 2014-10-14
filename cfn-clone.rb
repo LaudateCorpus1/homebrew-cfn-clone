322c91048eaf42d7247f5ddd76996f25d8a79293
Tommy Bishop <bishop.thomas@gmail.com>
2014-10-14 13:01:46 -0700

    add homebrew formula for 1.0.0

---
+++ cfn-clone.rb
@@ -0,0 +1,17 @@
+require 'formula'
+
+class CfnClone < Formula
+  homepage 'https://github.com/intuit/cfn-clone'
+  version '1.0.0'
+
+  url 'https://github.com/intuit/cfn-clone/releases/download/1.0.0/cfn-clone-1.0.0-darwin-amd64'
+  sha256 'c110c9e138a0bdb4e5e97e4fadbc8a449410fbf6e3bde214bae47395c7b06d1b'
+
+  head 'https://github.com/intuit/cfn-clone.git'
+  depends_on :arch => :intel
+
+  def install
+    bin.install Dir['*']
+  end
+
+end
