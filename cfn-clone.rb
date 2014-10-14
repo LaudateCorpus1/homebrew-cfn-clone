5a8b6b74399305551ead01185f78a9382d24340f
Tommy Bishop <bishop.thomas@gmail.com>
2014-10-14 14:17:28 -0700

    update to 1.0.1

--- cfn-clone.rb
+++ cfn-clone.rb
@@ -2,16 +2,15 @@ require 'formula'
 
 class CfnClone < Formula
   homepage 'https://github.com/intuit/cfn-clone'
-  version '1.0.0'
+  version '1.0.1'
 
-  url 'https://github.com/intuit/cfn-clone/releases/download/1.0.0/cfn-clone-1.0.0-darwin-amd64'
-  sha256 'c110c9e138a0bdb4e5e97e4fadbc8a449410fbf6e3bde214bae47395c7b06d1b'
+  url 'https://github.com/intuit/cfn-clone/releases/download/1.0.1/cfn-clone-1.0.1-darwin-amd64.tar.gz'
+  sha256 '3f4990be707bbd834b0e4eee5e30effde50da9c70bcfc7637bd7d83ce73e2d15'
 
   head 'https://github.com/intuit/cfn-clone.git'
   depends_on :arch => :intel
 
   def install
-    system "mv cfn-clone-*-darwin-amd64 cfn-clone"
     bin.install Dir['*']
   end
 
